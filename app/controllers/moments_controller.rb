ef688ccd7a0330144b1b962031d29a88edeb9333
Martin <hongzeqin@gmail.com>
2014-08-15 20:56:27 +0800

    controller scaffold

---
+++ app/controllers/moments_controller.rb
@@ -0,0 +1,13 @@
+class MomentsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def download
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'moments#new'
+
+  resources :moments do
+    get 'download', on: :member
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
